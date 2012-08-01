9e17a1d2b7d79fdbc362ddb543ef20a46e6171e3
lsujstans7 <jstans7@lsu.edu>
2012-08-01 14:34:25 -0500

    This is the old way of declaring

---
+++ Inventory/Item.h
@@ -0,0 +1,22 @@
+//
+//  Item.h
+//  Inventory
+//
+//  Created by iOS Camp on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+{
+    float price;
+    NSDate *releaseDate;
+}
+
+- (float)price;
+- (void)setPrice:(float)newPrice;
+
+- (NSDate *) releaseDate;
+- (void)setReleaseDate: (NSDate *)newDate;
+@end
---
+++ Inventory/Item.m
@@ -0,0 +1,30 @@
+//
+//  Item.m
+//  Inventory
+//
+//  Created by iOS Camp on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+- (float)price;
+{
+    return price;
+}
+- (void)setPrice:(float)newPrice;
+{
+    price = newPrice;
+}
+- (NSDate *) releaseDate;
+{
+    return releaseDate;
+}
+- (void)setReleaseDate: (NSDate *)newDate;
+{
+    releaseDate = newDate;
+}
+
+@end
